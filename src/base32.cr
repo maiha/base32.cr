9e0d06011c58b2a40eed1c6c9460c099c9face57
maiha <kazunori.nishi@supership.jp>
2021-03-29 23:02:20 +0900

    performance: use Char rather than String (100M: 55s->30s)

--- src/base32.cr
+++ src/base32.cr
@@ -1,11 +1,11 @@
 module Base32
   CHARS_SAFE = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
-  ENCODE_MAP = CHARS_SAFE.split(//)
+  ENCODE_MAP = CHARS_SAFE.chars
   DECODE_MAP = Hash.zip(ENCODE_MAP, (0..31).to_a)
 
   def self.encode(number : Int) : String
     return "0" if number == 0_i64
-    array = Array(String).new
+    array = Array(Char).new
     while number > 0_i64
       index  = number %  32_i64 # divmod is a bit slow
       number = number >> 5      # number / 32
@@ -16,7 +16,7 @@ module Base32
 
   def self.decode(string) : Int
     total = 0_i64
-    string.split(//).each_with_index do |char, i|
+    string.each_char_with_index do |char, i|
       value = DECODE_MAP[char]? || raise ArgumentError.new("Base32.decode([#{string}]) found unexpected char [#{char}] at [#{i}]")
       total = total * 32_i64 + value
     end
